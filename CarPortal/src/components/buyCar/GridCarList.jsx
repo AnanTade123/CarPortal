93252b1aeb33b23a3dd2b772672a15fdb43bc767
AmitOpticalArc <amitkuril@opticalarc.com>
2024-05-24 11:27:06 +0530

    url changes

--- CarPortal/src/components/buyCar/GridCarList.jsx
+++ CarPortal/src/components/buyCar/GridCarList.jsx
@@ -1,33 +1,32 @@
 /* eslint-disable no-unsafe-optional-chaining */
 /* eslint-disable react/prop-types */
 
-
 import { CardDefault } from "../../ui/CardDefault";
 import CardUi from "../../ui/CardUi";
 import { useEffect, useState } from "react";
 import InfiniteScroll from "react-infinite-scroll-component";
 
-const GridCarList = ({ data,error }) => {
-   console.log(data)
+const GridCarList = ({ data, error }) => {
+  console.log(data);
   const [posts, setPosts] = useState([]);
   const [page, setPage] = useState(0);
   const [lastPage, setLastPage] = useState();
-console.log(page)
+  console.log(page);
   useEffect(() => {
     if (data) {
       setPosts(data?.list);
     }
     if (error) {
-      alert('data not found');
+      alert("data not found");
     }
   }, [data, error]);
-console.log(page)
+  console.log(page);
   const fetchData = async () => {
     try {
       const nextPage = page + 1; // Increment page here
-      console.log(nextPage)
+      console.log(nextPage);
       const res = await fetch(
-        `https://carresel-production.up.railway.app/cars/mainFilter/${nextPage}`
+        `https://cf-production.up.railway.app/cars/mainFilter/${nextPage}`
       );
       const data = await res.json();
       console.log(data);
@@ -57,16 +56,14 @@ console.log(page)
             );
           })}
           <InfiniteScroll
-          dataLength={posts.length}
-          next={fetchData}
-          hasMore={true}
-          loader={<p>Loading...</p>}
-        scrollableTarget="scrollableDiv"
-        endMessage={lastPage && <p>You are in the last page</p>}
-
-        />
+            dataLength={posts.length}
+            next={fetchData}
+            hasMore={true}
+            loader={<p>Loading...</p>}
+            scrollableTarget="scrollableDiv"
+            endMessage={lastPage && <p>You are in the last page</p>}
+          />
         </div>
-      
       </CardUi>
     </>
   );
