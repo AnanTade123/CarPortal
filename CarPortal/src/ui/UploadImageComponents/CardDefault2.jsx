66e50b1a06d3a82889e52e57b5dca7ee56be7676
shubhaOARC24 <shubha.ukali@education.opticalarc.com>
2024-06-26 17:29:32 +0530

    Card images set

--- CarPortal/src/ui/UploadImageComponents/CardDefault2.jsx
+++ CarPortal/src/ui/UploadImageComponents/CardDefault2.jsx
@@ -8,9 +8,11 @@ export function CardDefault2({ data }) {
   const carid = data?.carId;
   console.log(carid);
   return (
-    <Card className="mt-6 w-80 md:m-0 items-center">
+    <Card className="mt-6 w-80 md:m-0 items-center max-w-[20rem] overflow-hidden">
       <div className="w-[18rem] h-[16rem] ">
+      <Link to={`/carlist/cardetails/${carid}`}>
         <CarouselCustomArrows carId={carid} />
+        </Link>
       </div>
       <CardBody className="mb-5">
         <Typography>{data.year}</Typography>
