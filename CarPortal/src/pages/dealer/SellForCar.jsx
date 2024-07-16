af2a8d8d964fadacc43465eca808fabede93862e
AnanTade123 <ananttade@education.opticalarc.com>
2024-07-16 08:15:49 +0530

    Commit

--- CarPortal/src/pages/dealer/SellForCar.jsx
+++ CarPortal/src/pages/dealer/SellForCar.jsx
@@ -1,3 +1,4 @@
+/* eslint-disable react-hooks/exhaustive-deps */
 /* eslint-disable no-unused-vars */
 import { useDealerIdByCarQuery } from "../../services/carAPI";
 import { Tooltip } from "@material-tailwind/react";
