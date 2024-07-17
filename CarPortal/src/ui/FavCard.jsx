59bf2ffcb03294b52d108c3b28d8563aa0251b17
shubhaOARC24 <shubha.ukali@education.opticalarc.com>
2024-07-17 18:27:08 +0530

    Shubha : FavCard issue resolved

--- CarPortal/src/ui/FavCard.jsx
+++ CarPortal/src/ui/FavCard.jsx
@@ -8,10 +8,16 @@ function FavCard({favoriteCarData}) {
   const navigate = useNavigate();
   const { carId } = favoriteCarData;
   const { data: cardata ,isLoading ,error } = useGetCarByIdQuery(carId);
+  
   if (isLoading) {
     return <p>Loading...</p>;
   }
-    const { object } = cardata;
+
+  const car = cardata?.object;
+
+  if (!car) {
+    return <></>;
+  }
   
  
   if (error?.status === 401) {
@@ -23,39 +29,39 @@ function FavCard({favoriteCarData}) {
     <div className="flex flex-wrap justify-center mx-auto lg:gap-5">
       <Card className="w-full flex justify-center sm:w-80 md:w-[260px] lg:w-fit items-center border-2 hover:scale-105 border-gray-300 shadow-xl overflow-hidden mx-5 md:mx-0">
         <CardHeader floated={false} shadow={false} color="transparent" className="m-0 rounded-none">
-          <Link to={`/carlist/cardetails/${object.carId}`}>
-            <CarouselCustomArrows carId={object.carId} />
+          <Link to={`/carlist/cardetails/${car.carId}`}>
+            <CarouselCustomArrows carId={car.carId} />
           </Link>
         </CardHeader>
         <CardBody className="mb-5">
-          <Typography>{object.year}</Typography>
+          <Typography>{car.year}</Typography>
           <Typography variant="h5" color="blue-gray" className="mb-2">
-            {object.brand} {object.model}
+            {car.brand} {car.model}
           </Typography>
           <Typography variant="h7" color="blue-gray" className="mb-2">
-            {object.title}
+            {car.title}
           </Typography>
           <p className="text-sm uppercase mb-3 flex flex-wrap gap-2">
             <span className="bg-gray-200 p-[5px] rounded-sm mr-2 text-black">
-              {object.kmDriven} KM
+              {car.kmDriven} KM
             </span>
             <span className="bg-gray-200 p-[5px] rounded-sm mr-2 text-black">
-              {object.fuelType}
+              {car.fuelType}
             </span>
             <span className="bg-gray-200 p-[5px] rounded-sm mr-2 text-black">
-              {object.transmission}
+              {car.transmission}
             </span>
           </p>
           <Typography variant="h6" className="font-bold text-black text-xl">
-            ₹ {object.price}
+            ₹ {car.price}
           </Typography>
-          <Link to={`/carlist/cardetails/${object.carId}`}>
+          <Link to={`/carlist/cardetails/${car.carId}`}>
             <button className="mt-2 mb-4 p-[7px] bg-indigo-500 rounded-lg text-white">
               View Car
             </button>
           </Link>
           <hr />
-          <p className="text-sm">Free Test Drive Today at {object.area}</p>
+          <p className="text-sm">Free Test Drive Today at {car.area}</p>
         </CardBody>
       </Card> 
     </div>
