90b888a4d7e1ba8e2b706dc494ec3d13ba0f646c
shubhaOARC24 <shubha.ukali@education.opticalarc.com>
2024-07-19 12:51:08 +0530

    Features of cars list show

--- CarPortal/src/components/carDetails/CarView.jsx
+++ CarPortal/src/components/carDetails/CarView.jsx
@@ -1,17 +1,10 @@
 /* eslint-disable no-unused-vars */
 /* eslint-disable react/prop-types */
 /* eslint-disable no-unsafe-optional-chaining */
-import { useState } from "react";
-import { useFilterCarQuery } from "../../services/carAPI";
-import GridCarList from "../buyCar/GridCarList";
+
 import CarView1 from "./CarView1";
-import InspectionReport from "./InspectionReport";
 import KnowYourCar from "./KnowYourCar";
 import TopFeatures from "./TopFeatures";
-import { Button } from "@material-tailwind/react";
-import { Link } from "react-router-dom";
-import Cookies from "js-cookie";
-import { jwtDecode } from "jwt-decode";
 
 const CarView = ({
   fuelType,
@@ -22,24 +15,11 @@ const CarView = ({
   carInsurance,
   kmDriven,
   carId,
-  beadingCarId
+  musicFeature,
+  acFeature,
+  powerWindowFeature,
+  rearParkingCameraFeature
 }) => {
-  console.log(
-    fuelType,
-    registration,
-    ownerSerial,
-    transmission,
-    year,
-    carInsurance,
-    kmDriven
-  );
-  const token = Cookies.get("token");
-let jwtDecodes;
-if (token) {
-  jwtDecodes = jwtDecode(token);
-}
-
-const userRole = token ? jwtDecodes?.authorities[0] : null;
   return (
     <div>
       <CarView1 carId = {carId} />
@@ -55,7 +35,12 @@ const userRole = token ? jwtDecodes?.authorities[0] : null;
 
       {/* <InspectionReport/> */}
       
-      {/* <TopFeatures/> */}
+      <TopFeatures
+      acFeature={acFeature}
+      musicFeature={musicFeature}
+      powerWindowFeature={powerWindowFeature}
+      rearParkingCameraFeature={rearParkingCameraFeature}
+      />
     </div>
   );
 };
