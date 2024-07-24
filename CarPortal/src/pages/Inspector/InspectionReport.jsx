5f4ec2f6f79f5bc8004df27921141b2c7deb93b7
AnanTade123 <ananttade@education.opticalarc.com>
2024-07-24 19:19:45 +0530

    bug remove

--- CarPortal/src/pages/Inspector/InspectionReport.jsx
+++ CarPortal/src/pages/Inspector/InspectionReport.jsx
@@ -105,7 +105,7 @@ console.log(formData)
     
       toast.success("Data Uploaded");
       setTimeout(() => {
-        navigate("/sales/biddingcar")
+        navigate("/inspector/car")
       },1000)
      
   } catch (error) {
