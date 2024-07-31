5aad30ec695882abafd8f103c0f3922758f2b9ce
AnanTade123 <ananttade@education.opticalarc.com>
2024-07-31 15:53:39 +0530

    Some changes

--- CarPortal/src/ui/WinnerSectionImages.jsx
+++ CarPortal/src/ui/WinnerSectionImages.jsx
@@ -1,36 +1,29 @@
 /* eslint-disable react/prop-types */
-// import { Carousel, IconButton } from "@material-tailwind/react";
 
-// eslint-disable-next-line react/prop-types
 export function WinnerSectionImages({ ImageData }) {
-    console.log(ImageData);
+  console.log(ImageData);
+
   
-    if (!ImageData || !ImageData.object || ImageData.object.length === 0) {
-      return (
-        <div className="text-center mt-5">
-          <img
-            className="h-[14rem] w-[19rem] p-[20px] opacity-50"
-            src="..\..\cars\no-image-available.png"
-            alt="no image"
-          />
-        </div>
-      );
-    }
-  
+  const coverImage = ImageData.object.find(item => item.documentType === "coverImage");
+
+  if (!coverImage) {
     return (
-      <>
-        {ImageData.object.map((item) =>
-          item.documentType === "coverImage" ? (
-            <img
-              key={item.documentId}
-              src={item.documentLink}
-              alt={`Car Image ${item?.documentId}`}
-              className="rounded-lg h-[15rem] w-[19rem]"
-            />
-          ) : null
-        )}
-        {/* // </Carousel> */}
-      </>
+      <div className="text-center mt-5">
+        <img
+          className="h-[14rem] w-[19rem] p-[20px] opacity-50"
+          src="../../cars/no-image-available.png"
+          alt="No image available"
+        />
+      </div>
     );
   }
-  
+
+  return (
+    <img
+      key={coverImage.documentId}
+      src={coverImage.documentLink}
+      alt={`Car Image ${coverImage.documentId}`}
+      className="rounded-lg h-[15rem] w-[19rem]"
+    />
+  );
+}
