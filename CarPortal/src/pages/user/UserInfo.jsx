f39e9c30f6e7a219f94da5848cc5fa3a534dcd34
Sumedhk18 <sumedhkamalapurkar@opticalarc.com>
2024-08-01 18:08:52 +0530

    Sumedh-userProfileUi

--- CarPortal/src/pages/user/UserInfo.jsx
+++ CarPortal/src/pages/user/UserInfo.jsx
@@ -31,11 +31,11 @@ const UserInfo = () => {
 
   return (
     <>
-      <div className="text-3xl font-bold mt-10 ml-16 mb-[-5rem]">
+      <div className="text-3xl font-bold mt-5 md:ml-16 md:mb-[-5rem]">
        User Information
       </div>
       <div className="flex justify-center items-center h-screen">
-        <div className="w-full max-w-4xl flex flex-col md:flex-row shadow-lg">
+        <div className="w-full max-w-4xl flex flex-col md:flex-row shadow-xl">
           <div className="w-full md:w-1/2">
             <img
               src="https://www.shutterstock.com/image-photo/smiling-friendly-car-seller-suit-600nw-2105619599.jpg"
@@ -45,7 +45,7 @@ const UserInfo = () => {
           </div>
           <div className="w-full md:w-1/2 p-8 flex flex-col justify-between">
             <div>
-              <table className="table w-full ml-2 border-collapse border border-gray-200">
+              <table className="table w-full -mt-8 mb-5 ml-2 border-collapse border border-gray-200">
                 <tbody>
                   <tr>
                     <th className="px-4 py-2 border border-gray-200">
