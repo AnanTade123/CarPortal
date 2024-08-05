9698512a2c4456b58a02599692d64c2c4d0cee99
shubhaOARC24 <shubha.ukali@education.opticalarc.com>
2024-08-05 17:29:30 +0530

    Shubha :Replace title in sale person form document ID to Adhar No

--- CarPortal/src/components/admin/AddSalesForm.jsx
+++ CarPortal/src/components/admin/AddSalesForm.jsx
@@ -197,7 +197,7 @@ export function AddSalesForm() {
                 required
               />
               <Input
-                label="Document Id"
+                label="Adhar No"
                 name="documentId"
                 value={formData.documentId}
                 onChange={handleChange}
