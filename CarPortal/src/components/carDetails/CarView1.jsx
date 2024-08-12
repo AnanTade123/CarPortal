7905b1760b89c025f94020d8d094f2220f9a500c
AmitOpticalArc <amitkuril@opticalarc.com>
2024-08-12 18:12:58 +0530

    Amit - Changes in CarView1 and Filter Component

--- CarPortal/src/components/carDetails/CarView1.jsx
+++ CarPortal/src/components/carDetails/CarView1.jsx
@@ -1,5 +1,5 @@
 /* eslint-disable no-unused-vars */
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import { Carousel } from "@material-tailwind/react";
 
 import Exterior from "/cars/Exterior.webp";
@@ -19,10 +19,42 @@ const CarView1 = ({ carId }) => {
   const [features, setFeatures] = useState(false);
   const [tyres, setTyres] = useState(false);
   const [engines, setEngines] = useState(false);
-  console.log(exterior);
+  const [imageURLs, setImageURLs] = useState([]);
+  // console.log(exterior);
 
   const { data, isLoading, error } = useGetCarImageByIdQuery({ carId });
-  console.log(data);
+  // console.log(data);
+
+  const checkIfImage = (url) => {
+    return new Promise((resolve) => {
+      const img = new Image();
+      img.onload = () => resolve(url); // Return the original URL if the image loads
+      img.onerror = () => resolve(fallbackImage); // Return the fallback image if the image fails to load
+      img.src = url;
+    });
+  };
+  const fallbackImage = "..\\..\\cars\\no-image-available-update.png";
+  useEffect(() => {
+    if (data?.object && Array.isArray(data.object)) {
+      // Reorder data to have 'coverImage' first
+      const reorderedData = [
+        ...data.object.filter((item) => item.documentType === "coverImage"),
+        ...data.object.filter((item) => item.documentType !== "coverImage"),
+      ];
+
+      // Extract the URLs from the reordered data
+      const urls = reorderedData.map((item) => item.documentLink);
+      // console.log(urls)
+      // Validate URLs and set image URLs
+      Promise.all(urls.map(checkIfImage)).then((validatedURLs) =>
+        setImageURLs(validatedURLs)
+      );
+    } else {
+      setImageURLs([fallbackImage]); // Handle case when data.object is not valid
+    }
+  }, [data]);
+
+  // console.log(imageURLs)
 
   if (isLoading) return <div>Loading...</div>;
   if (error)
@@ -32,12 +64,14 @@ const CarView1 = ({ carId }) => {
         <div className="flex justify-center">
           <img
             className=" md:w-[12rem] w-[10rem] opacity-50 "
-            src="..\..\cars\no-image-available.png"
+            src="..\..\cars\no-image-available-update.png"
             alt="no image"
           />
         </div>
       </div>
     );
+
+  //  console.log(imageURLs)
 
   const ExteriorImages = data.object.filter(
     (item) => item.documentType === "Exterior"
@@ -52,8 +86,6 @@ const CarView1 = ({ carId }) => {
   const EngineImages = data.object.filter(
     (item) => item.documentType === "Engine"
   );
-
-  const fallbackImage = "..\\..\\cars\\no-image-available.png";
 
   const ChooseCarColor = () => {
     return (
@@ -83,18 +115,17 @@ const CarView1 = ({ carId }) => {
             className="bg-white rounded-lg shadow-md absolute inset-0 blurred-bg"
             style="background-image: url('https://images.pexels.com/photos/1545743/pexels-photo-1545743.jpeg?auto=compress&cs=tinysrgb&w=600');"
           >
-            {data.object.length > 0 ? (
-              data.object.map((item) => (
-                // <div key={item.documentId} className="absolute inset-0 blurred-bg" style="background-image: url('https://images.pexels.com/photos/1545743/pexels-photo-1545743.jpeg?auto=compress&cs=tinysrgb&w=600');">
-                <img
-                  key={item.documentId}
-                  src={item.documentLink}
-                  alt={`Car Image ${item.documentId}`}
-                  onError={(e) => (e.currentTarget.src = fallbackImage)}
-                  className="object-contain w-full h-full relative z-10 "
-                />
-                // </div>
-              ))
+            {imageURLs.length > 0 ? (
+              <Carousel className="bg-white rounded-lg shadow-md">
+                {imageURLs.map((url, index) => (
+                  <img
+                    key={index}
+                    src={url}
+                    alt={`Car Image ${index}`}
+                    className="object-contain w-full h-full opacity-50"
+                  />
+                ))}
+              </Carousel>
             ) : (
               <div className="text-center mt-2">
                 {" "}
@@ -109,7 +140,7 @@ const CarView1 = ({ carId }) => {
           </Carousel>
         )}
 
-{exterior && (
+        {exterior && (
           <Carousel className="bg-white rounded-lg shadow-md ">
             {imageURLs.length > 0 ? (
               imageURLs.map((url, index) => (
@@ -147,8 +178,8 @@ const CarView1 = ({ carId }) => {
 
         {features && (
           <Carousel className="bg-white rounded-lg shadow-md ">
-            {data.object.length > 0 ? (
-              data.object.map((item) => (
+            {imageURLs.length > 0 ? (
+              imageURLs.map((item) => (
                 <img
                   key={item.documentId}
                   src={item.documentLink}
@@ -172,15 +203,17 @@ const CarView1 = ({ carId }) => {
 
         {tyres && (
           <Carousel className="bg-white rounded-lg shadow-md">
-            {data.object.length > 0 ? (
-              data.object.map((item) => (
-                <img
-                  key={item.documentId}
-                  src={item.documentLink}
-                  alt={`Car Image ${item.documentId}`}
-                  className="object-contain w-full h-full"
-                />
-              ))
+            {imageURLs.length > 0 ? (
+              <Carousel className="bg-white rounded-lg shadow-md">
+                {imageURLs.map((url, index) => (
+                  <img
+                    key={index}
+                    src={url}
+                    alt={`Car Image ${index}`}
+                    className="object-contain w-full h-full"
+                  />
+                ))}
+              </Carousel>
             ) : (
               <div className="text-center mt-2">
                 {" "}
@@ -197,15 +230,17 @@ const CarView1 = ({ carId }) => {
 
         {engines && (
           <Carousel className="bg-white rounded-lg shadow-md  ">
-            {data.object.length > 0 ? (
-              data.object.map((item) => (
-                <img
-                  key={item.documentId}
-                  src={item.documentLink}
-                  alt={`Car Image ${item.documentId}`}
-                  className="object-contain w-full h-full"
-                />
-              ))
+            {imageURLs.length > 0 ? (
+              <Carousel className="bg-white rounded-lg shadow-md">
+                {imageURLs.map((url, index) => (
+                  <img
+                    key={index}
+                    src={url}
+                    alt={`Car Image ${index}`}
+                    className="object-contain w-full h-full"
+                  />
+                ))}
+              </Carousel>
             ) : (
               <div className="text-center mt-2">
                 {" "}
@@ -213,7 +248,7 @@ const CarView1 = ({ carId }) => {
                 <img
                   src={fallbackImage}
                   alt="no image"
-                  className="object-contain h-full md:w-[12rem] w-[13rem] ml-12 md:ml-[9rem] mt-8 "
+                  className="object-contain h-full md:w-[12rem] w-[13rem] ml-12 md:ml-[9rem] mt-8 opacity-0 "
                 />
               </div>
             )}
