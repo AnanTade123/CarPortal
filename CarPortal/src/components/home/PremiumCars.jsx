a03246f1543cba110436eb53a6fb8815d1ca6118
Rohan3275 <rohanshrivastav@opticalarc.com>
2024-08-13 23:49:38 +0530

    changes

--- CarPortal/src/components/home/PremiumCars.jsx
+++ CarPortal/src/components/home/PremiumCars.jsx
@@ -4,50 +4,11 @@ import pre4 from "/cars/pre4.jpg";
 import pre5 from "/cars/pre5.jpg";
 import pre6 from "/cars/pre6.jpg";
 import BrandList2 from "./BrandList2";
-import { PrimiumNavbar } from "../navbars/PrimiumNavbar";
+import  {PrimiumNavbar} from "../navbars/PrimiumNavbar";
 import FooterF from "../Footer";
 
 const PremiumCars = () => {
-    return (
-        <div>
-            <Carousel
-                className=" overflow-hidden"
-                autoplay
-                autoplayDelay={3000}
-                loop
-            >
-               <div className="relative overflow-hidden">
-               
-                    <img src={pre4} alt="Image 1" className="object w-full md:h-full h-[200px]" />
-                    <div className="absolute grid inset-0 bg-black/40">
-                    <div className=" mt-2 md:mt-4 xl:mt-8 md:ml-10 mx-5 ">
-                        <div className="text-white text-lg md:text-xl lg:text-4xl  mb-2  font-bold  rounded">                    
-                            Premium Cars Collection...
-                        </div>
-                        <p className="text-white font-bold mb-5  text-xs lg:w-2/3 md:text-xl rounded">
-                            Car Is Where Early Adopters And Innovation Seekers Lively Imaginative Tech Before It Hits The Mainstream
-                        </p>
-                    <Link to="/PremiumCarList">
-                        <button className="p-1 md:p-4 rounded-md bg-white">
-                            <h1 className="text-center text-xs md:text-sm font-semibold"> GO TO LISTING </h1>
-                        </button>
-                    </Link>
-                
-                    </div>
-                </div>
-                  
-                   
-                </div>
-
-                <div className="relative">
-               
-               <img src={pre5} alt="Image 1" className="object-cover w-full md:h-full h-[200px] " />
-               <div className="absolute grid inset-0 bg-black/40">
-               <div className=" mt-2 md:mt-4 xl:mt-8 ml-10">
-                   <span className="text-white  text-md md:text-xl lg:text-4xl mb-2 font-bold  rounded">                    
-                   Redefining The Standards Of Luxury
-                   </span>
-                   {/* <p className="text-white font-bold mb-5 rounded">
+    
   return (
     <div>
       <PrimiumNavbar></PrimiumNavbar>
@@ -68,10 +29,14 @@ const PremiumCars = () => {
               <div className="text-white text-lg md:text-xl lg:text-4xl  mb-2  font-bold  rounded md:mt-[70px] mt-14 ">
                 Premium Cars Collection...
               </div>
-              <p className="text-white font-bold mb-5  text-xs lg:w-2/3 md:text-xl rounded ">
-                Car Is Where Early Adopters And Innovation Seekers Lively
-                Imaginative Tech Before It Hits The Mainstream
-              </p>
+              <p className="text-white font-bold mb-5  text-xs lg:w-2/3 md:text-xl rounded">
+                            Car Is Where Early Adopters And Innovation Seekers Lively Imaginative Tech Before It Hits The Mainstream
+                        </p>
+                         <Link to="/PremiumCarList">
+                        <button className="p-1 md:p-4 rounded-md bg-white">
+                            <h1 className="text-center text-xs md:text-sm font-semibold"> GO TO LISTING </h1>
+                        </button>
+                    </Link>
             </div>
           </div>
         </div>
