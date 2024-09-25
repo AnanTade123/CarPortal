291c2039b4abc8d22676637712d88437e3884839
shubhaOARC24 <shubha.ukali@education.opticalarc.com>
2024-09-25 19:47:35 +0530

    Shubha : color set

--- CarPortal/src/pages/dealer/AddDealerCar2.jsx
+++ CarPortal/src/pages/dealer/AddDealerCar2.jsx
@@ -73,7 +73,7 @@ export default function AddDealerCar() {
   const { data: variantData } = useGetVariantsQuery(selectedBrand, {
     skip: !selectedBrand,
   });
-  // console.log(variantData);
+  console.log("variantData");
   const { data: subVariantData } = useGetSubVariantsQuery(
     { brand: selectedBrand, variant: selectedModel },
     {
